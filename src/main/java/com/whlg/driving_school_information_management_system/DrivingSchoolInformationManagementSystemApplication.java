75eb943f619f12e77076b40fac6596c75fcc4aa6
charile_Lu <www.1072571694@qq.com>
2020-02-16 20:02:20 +0800

    第一次提交

---
+++ src/main/java/com/whlg/driving_school_information_management_system/DrivingSchoolInformationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.whlg.driving_school_information_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolInformationManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivingSchoolInformationManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whlg/driving_school_information_management_system/DrivingSchoolInformationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whlg.driving_school_information_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolInformationManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
